e4272055c9ee5ac334c8f9f2cee1554d17f0ecf3
Marius Žakevičius <marius.zakevicius@alchemetrics.co.uk>
2019-09-25 13:04:11 +0300

    Styling

--- zindymokelias/src/components/posts/PostItem.js
+++ zindymokelias/src/components/posts/PostItem.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import Button from '../elements/Button';
-import image from '../../img/img-3.JPG'
+import React from "react";
+import Button from "../elements/Button";
+import image from "../../img/img-3.JPG";
 
 const PostItem = () => {
   return (
     <div className="post flex wrap">
       <img src={image} alt="woman with donkey" className="post_image" />
-      <div className="post_content">
+      <div className="post_content flex wrap">
         <h2>POST TITLE</h2>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nemo aut sed facilis maxime quo odit nulla voluptatum quas voluptas! Quaerat excepturi, natus maiores fuga laudantium repudiandae dicta asperiores optio. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque qui laboriosam consectetur ipsam, magni recusandae nisi vero? Libero, beatae perferendis esse aliquam vel optio minima molestiae, reiciendis eius velit odio!</p>
-      </div>
-      <div className="post_buttons flex right-X">
-        <Button text='Daugiau...' type='secondary' />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quis nemo
+          aut sed facilis maxime quo odit nulla voluptatum quas voluptas!
+          Quaerat excepturi, natus maiores fuga laudantium repudiandae dicta
+          asperiores optio. Lorem ipsum, dolor sit amet consectetur adipisicing
+          elit. Doloremque qui laboriosam consectetur ipsam, magni recusandae
+          nisi vero? Libero, beatae perferendis esse aliquam vel optio minima
+          molestiae, reiciendis eius velit odio!
+        </p>
+        <div className="post_buttons flex">
+          <Button text="Daugiau..." type="secondary" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PostItem
+export default PostItem;
