28f57ec8027b53bd34ac85f4e6d5bd53e354403c
Marius Žakevičius <marius.zakevicius@alchemetrics.co.uk>
2019-11-08 22:42:42 +0200

    Changes for IE support

--- Logway/js/content.js
+++ Logway/js/content.js
@@ -1,218 +1,266 @@
 var contentLT = {
-    headerLinks: [
-        ['Ką mes siūlome?', '#bodyCont'],
-        ['Susisiekim', '#form']
+  headerLinks: [["Ką mes siūlome?", "#bodyCont"], ["Susisiekim", "#form"]],
+  customsContent: [
+    "Importo procedūros",
+    "Eksporto procedūros",
+    "Tranzito procedūros",
+    "Tarpininkavimas muitinėje",
+    "Muitinės sandėliavimo procedūros",
+    "Važtaraščių ir TIR knygelių pildymas"
+  ],
+  warehouseContent: [
+    "Sandėliavimas",
+    "Krovos darbai",
+    "Muitinės sandėliavimas",
+    "Papildomi sandėlio darbai",
+    "Krovinio apdirbimo darbai",
+    "Nestandartinių krovinių krovimas"
+  ],
+  formContent: {
+    contactTitle: "Susisiekite su mumis",
+    contactText: [
+      "Jei turite klausimų, drąsiai kreipkitės.",
+      "Atsakysim, patarsim, padėsim."
     ],
-    customsContent: ['Importo procedūros', 'Eksporto procedūros', 'Tranzito procedūros', 'Tarpininkavimas muitinėje', 'Muitinės sandėliavimo procedūros', 'Važtaraščių ir TIR knygelių pildymas'],
-    warehouseContent: ['Sandėliavimas', 'Krovos darbai', 'Muitinės sandėliavimas', 'Papildomi sandėlio darbai', 'Krovinio apdirbimo darbai', 'Nestandartinių krovinių krovimas'],
-    formContent: {
-        contactTitle: 'Susisiekite su mumis',
-        contactText: ['Jei turite klausimų, drąsiai kreipkitės.', 'Atsakysim, patarsim, padėsim.'],
-        formFields: {
-            name: ['Vardas', 'Jūsų vardas', 'text', 'name'],
-            email: ['E-mail', 'Jūsų e-mail', 'email', 'email'],
-            msg: ['Žinutė', 'Jūsų žinutė', 'textarea', 'message'],
-            button: ['submit', 'Siųsti']
-        }
-    },
-    workingHours: {
-        title: 'Darbo laikas',
-        workDays: ['Pr', 'An', 'Tr', 'Kt', 'Pn', 'Š', 'S']
-    },
-    address: {
-        first: 'Galinės g. 1, korp. C, 310b kab.',
-        second: 'Galinės k., Avižienių sen.',
-        third: 'Vilniaus raj., LT-14247',
-        phone: '+37062176494',
-        email: 'info@logway1.lt'
-    },
-    footer: 'Visos teisės saugomos &copy; '
+    formFields: {
+      name: ["Vardas", "Jūsų vardas", "text", "name"],
+      email: ["E-mail", "Jūsų e-mail", "email", "email"],
+      msg: ["Žinutė", "Jūsų žinutė", "textarea", "message"],
+      button: ["submit", "Siųsti"]
+    }
+  },
+  workingHours: {
+    title: "Darbo laikas",
+    workDays: ["Pr", "An", "Tr", "Kt", "Pn", "Š", "S"]
+  },
+  address: {
+    first: "Galinės g. 1, korp. C, 310b kab.",
+    second: "Galinės k., Avižienių sen.",
+    third: "Vilniaus raj., LT-14247",
+    phone: "+37062176494",
+    email: "info@logway1.lt"
+  },
+  footer: "Visos teisės saugomos &copy; "
 };
 
 var contentRU = {
-    headerLinks: [
-        ['Что мы предлагаем?', '#bodyCont'],
-        ['Пообщаемся', '#form']
+  headerLinks: [["Что мы предлагаем?", "#bodyCont"], ["Пообщаемся", "#form"]],
+  customsContent: [
+    "Импортные процедуры",
+    "Экспортные процедуры",
+    "Транзитные процедуры",
+    "Таможенное оформление",
+    "Таможенные складские процедуры",
+    "Заполнения ЦМР и книжек МДП"
+  ],
+  warehouseContent: [
+    "Складирование",
+    "Грузовые операции",
+    "Таможенное складирование",
+    "Дополнительные складские работы",
+    "Обработка грузов",
+    "Крупногабаритные и негабаритные грузы"
+  ],
+  formContent: {
+    contactTitle: "Свяжитесь с нами",
+    contactText: [
+      "Возникли вопросы? Обращайтесь - ответим, посоветуем, поможем."
     ],
-    customsContent: ['Импортные процедуры', 'Экспортные процедуры', 'Транзитные процедуры', 'Таможенное оформление', 'Таможенные складские процедуры', 'Заполнения ЦМР и книжек МДП'],
-    warehouseContent: ['Складирование', 'Грузовые операции', 'Таможенное складирование', 'Дополнительные складские работы', 'Обработка грузов', 'Крупногабаритные и негабаритные грузы'],
-    formContent: {
-        contactTitle: 'Свяжитесь с нами',
-        contactText: ['Возникли вопросы? Обращайтесь - ответим, посоветуем, поможем.'],
-        formFields: {
-            name: ['Имя', 'Ваше имя', 'text', 'name'],
-            email: ['E-почта', 'Ваш адрес электронной почты', 'email', 'email'],
-            msg: ['Сообщение', 'Ваше сообщение', 'textarea', 'message'],
-            button: ['submit', 'Oтправить']
-        }
-    },
-    workingHours: {
-        title: 'Рабочее время',
-        workDays: ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
-    },
-    address: {
-        first: 'ул. Галинес, 1, корп. Ц, 310б каб.',
-        second: 'д. Галинес, Авиженяй.',
-        third: 'Вильнюсский район., ЛТ-14247',
-        phone: '+37062176494',
-        email: 'info@logway1.lt'
-    },
-    footer: 'все права защищены. &copy; '
+    formFields: {
+      name: ["Имя", "Ваше имя", "text", "name"],
+      email: ["E-почта", "Ваш адрес электронной почты", "email", "email"],
+      msg: ["Сообщение", "Ваше сообщение", "textarea", "message"],
+      button: ["submit", "Oтправить"]
+    }
+  },
+  workingHours: {
+    title: "Рабочее время",
+    workDays: ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
+  },
+  address: {
+    first: "ул. Галинес, 1, корп. Ц, 310б каб.",
+    second: "д. Галинес, Авиженяй.",
+    third: "Вильнюсский район., ЛТ-14247",
+    phone: "+37062176494",
+    email: "info@logway1.lt"
+  },
+  footer: "все права защищены. &copy; "
 };
 
 var contentEN = {
-    headerLinks: [
-        ['What we offer?', '#bodyCont'],
-        ['Contact us', '#form']
+  headerLinks: [["What we offer?", "#bodyCont"], ["Contact us", "#form"]],
+  customsContent: [
+    "Import procedures",
+    "Export procedures",
+    "Transit procedures",
+    "Customs Brokerage",
+    "Customs warehouses",
+    "Filling of CMR and TIR Carnets"
+  ],
+  warehouseContent: [
+    "Warehousing",
+    "Loading/unloading operations",
+    "Customs warehousing",
+    "Custom cargo operations",
+    "Additional cargo processing works",
+    "Bulky cargo operations"
+  ],
+  formContent: {
+    contactUs: "Contact Us",
+    contactHelp: [
+      "Have any questions? Feel free to contact us",
+      "We are here to answer, advice and help."
     ],
-    customsContent: ['Import procedures', 'Export procedures', 'Transit procedures', 'Customs Brokerage', 'Customs warehouses', 'Filling of CMR and TIR Carnets'],
-    warehouseContent: ['Warehousing', 'Loading/unloading operations', 'Customs warehousing', 'Custom cargo operations', 'Additional cargo processing works', 'Bulky cargo operations'],
-    formContent: {
-        contactUs: 'Contact Us',
-        contactHelp: ['Have any questions? Feel free to contact us', 'We are here to answer, advice and help.'],
-        formFields: {
-            name: ['Name', 'Your name', 'text', 'name'],
-            email: ['E-mail', 'Your e-mail', 'email', 'email'],
-            msg: ['Message', 'Your message', 'textarea', 'message'],
-            button: ['submit', 'Send']
-        }
-    },
-    workingHours: {
-        title: 'Working hours',
-        workDays: ['Mon.', 'Tue.', 'Wed.', 'Thu.', 'Fri.', 'Sat.', 'Sun.']
-    },
-    address: {
-        first: 'Galinės str.. 1, corp. C, room 310b',
-        second: 'Galinės village, Avižieniai',
-        third: 'Vilnius region, LT-14247',
-        phone: '+37062176494',
-        email: 'info@logway1.lt'
-    },
-    footer: 'All rights reserved &copy; '
+    formFields: {
+      name: ["Name", "Your name", "text", "name"],
+      email: ["E-mail", "Your e-mail", "email", "email"],
+      msg: ["Message", "Your message", "textarea", "message"],
+      button: ["submit", "Send"]
+    }
+  },
+  workingHours: {
+    title: "Working hours",
+    workDays: ["Mon.", "Tue.", "Wed.", "Thu.", "Fri.", "Sat.", "Sun."]
+  },
+  address: {
+    first: "Galinės str.. 1, corp. C, room 310b",
+    second: "Galinės village, Avižieniai",
+    third: "Vilnius region, LT-14247",
+    phone: "+37062176494",
+    email: "info@logway1.lt"
+  },
+  footer: "All rights reserved &copy; "
 };
 
+var loc = window.location;
 
-
-var hrefArr = window.location.href.split('=');
+var language = loc.href.slice(loc.href.indexOf("=") + 1);
 var content = contentLT;
 
-if (!hrefArr[1]) {
+console.log(language);
+
+if (language === "") {
   content = contentLT;
-} else if (hrefArr[1].includes('EN')) {
-    content = contentEN;
-} else if (hrefArr[1].includes('RU')) {
-    content = contentRU;
+} else if (language === "EN") {
+  content = contentEN;
+} else if (language === "RU") {
+  content = contentRU;
 } else {
-    content = contentLT;
+  content = contentLT;
 }
 
-var whi = 0 // itterator for working Houst display
+var whi = 0; // itterator for working Houst display
 
 // FILLING PAGE WITH TEXT BASED ON LANGUAGE SELECTED
 
-var customsList = document.querySelector('.customs ul');
-var warehouseList = document.querySelector('.warehouse ul');
-var navigation = document.querySelector('#navigation');
-var formDiv = document.querySelector('#form .contactUs');
-var form = document.querySelector('form');
-var address = document.querySelector('.address');
-var schedule = document.querySelector('#workDays');
-var markup = '';
+var customsList = document.querySelector(".customs ul");
+var warehouseList = document.querySelector(".warehouse ul");
+var navigation = document.querySelector("#navigation");
+var formDiv = document.querySelector("#form .contactUs");
+var form = document.querySelector("form");
+var address = document.querySelector(".address");
+var schedule = document.querySelector("#workDays");
+var markup = "";
 
-content.headerLinks.forEach(createMarkup.bind(null, 'link'));
-navigation.insertAdjacentHTML('afterbegin', markup)
-markup = '';
+content.headerLinks.forEach(createMarkup.bind(null, "link"));
+navigation.insertAdjacentHTML("afterbegin", markup);
+markup = "";
 
-content.customsContent.forEach(createMarkup.bind(null, 'list'));
-customsList.insertAdjacentHTML('afterbegin', markup);
-markup = '';
+content.customsContent.forEach(createMarkup.bind(null, "list"));
+customsList.insertAdjacentHTML("afterbegin", markup);
+markup = "";
 
-content.warehouseContent.forEach(createMarkup.bind(null, 'list'));
-warehouseList.insertAdjacentHTML('afterbegin', markup);
-markup = '';
+content.warehouseContent.forEach(createMarkup.bind(null, "list"));
+warehouseList.insertAdjacentHTML("afterbegin", markup);
+markup = "";
 
-Object.keys(content.formContent).forEach(function (key) {
-    if (Array.isArray(content.formContent[key])) {
-        content.formContent[key].forEach(createMarkup.bind(null, 'formText'));
-        formDiv.insertAdjacentHTML('beforeend', markup);
-        markup = '';
-    } else if (typeof (content.formContent[key]) === 'object') {
-        Object.keys(content.formContent[key]).forEach(function (skey) {
-            if (skey !== 'button') {
-                createMarkup('formFields', content.formContent[key][skey]);
-            } else {
-                createMarkup('button', content.formContent[key][skey]);
-            }
-        });
-        form.insertAdjacentHTML('afterbegin', markup);
-    } else if (typeof (content.formContent[key]) === 'string') {
-        createMarkup('formTitle', content.formContent[key]);
-        formDiv.insertAdjacentHTML('afterbegin', markup);
-        markup = '';
-    }
+Object.keys(content.formContent).forEach(function(key) {
+  if (Array.isArray(content.formContent[key])) {
+    content.formContent[key].forEach(createMarkup.bind(null, "formText"));
+    formDiv.insertAdjacentHTML("beforeend", markup);
+    markup = "";
+  } else if (typeof content.formContent[key] === "object") {
+    Object.keys(content.formContent[key]).forEach(function(skey) {
+      if (skey !== "button") {
+        createMarkup("formFields", content.formContent[key][skey]);
+      } else {
+        createMarkup("button", content.formContent[key][skey]);
+      }
+    });
+    form.insertAdjacentHTML("afterbegin", markup);
+  } else if (typeof content.formContent[key] === "string") {
+    createMarkup("formTitle", content.formContent[key]);
+    formDiv.insertAdjacentHTML("afterbegin", markup);
+    markup = "";
+  }
 });
-markup = '';
-
+markup = "";
 
 Object.keys(content.address).forEach(function(key) {
-  if (key === 'phone') {
-    createMarkup('addressPhone', content.address[key]);
-  } else if (key === 'email') {
-    createMarkup('addressEmail', content.address[key]);
+  if (key === "phone") {
+    createMarkup("addressPhone", content.address[key]);
+  } else if (key === "email") {
+    createMarkup("addressEmail", content.address[key]);
   } else {
-    createMarkup('formText', content.address[key]);
+    createMarkup("formText", content.address[key]);
   }
 });
-address.insertAdjacentHTML('afterbegin', markup);
-markup = '';
+address.insertAdjacentHTML("afterbegin", markup);
+markup = "";
 
 Object.keys(content.workingHours).forEach(function(key) {
-  if (key === 'title') {
-    createMarkup('workHoursTitle', content.workingHours[key]);
+  if (key === "title") {
+    createMarkup("workHoursTitle", content.workingHours[key]);
   } else {
-    content.workingHours[key].forEach(createMarkup.bind(null, 'workHoursInfo'));
+    content.workingHours[key].forEach(createMarkup.bind(null, "workHoursInfo"));
   }
 });
-schedule.insertAdjacentHTML('afterbegin', markup);
-markup = '';
-
+schedule.insertAdjacentHTML("afterbegin", markup);
+markup = "";
 
 // CREATING HTML
 
 function createMarkup(tagName, el) {
-    if (tagName === 'list') {
-        markup += '<li><h2 class="h2">' + el + '</h2></li>';
-    } else if (tagName === 'link') {
-        markup += '<a href="' + el[1] + '">' + el[0] + '</a>';
-    } else if (tagName === 'formTitle') {
-        markup += '<h2>' + el + '</h2>';
-    } else if (tagName === 'formText') {
-        markup += '<p>' + el + '</p>';
-    } else if (tagName === 'formFields') {
-        markup += '<h2>' + el[0] + '</h2>';
-        if (el[2] !== 'textarea') {
-            markup += '<input type="' + el[2] + '" name="' + el[3] + '" placeholder="' + el[1] + '"/>';
-        } else {
-            markup += '<textarea rows=5 name="' + el[3] + '"></textarea>';
-        }
-    } else if (tagName === 'addressPhone') {
-        markup += '<p><a href="tel:' + el + '">' + el + '</a></p>';
-    } else if (tagName === 'addressEmail') {
-        markup += '<p><a href="mailto:' + el + '">' + el + '</a></p>';
-    } else if (tagName === 'workHoursTitle') {
-      markup += '<caption>' + el + '</caption>';
-    } else if (tagName === 'workHoursInfo') {
-      if (whi === 0) {
-        markup += '<tr><td>' + el + '</td><td rowspan="5">09.00-19.00</td></tr>';
-        whi++;
-      } else if (whi === 5) {
-        markup += '<tr><td>' + el + '</td><td rowspan="2">09.00-15.00</td></tr>';
-        whi++;
-      } else {
-        markup += '<tr><td>' + el + '</td></tr>';
-        whi++;
-      }
-    } else if ('button') {
-        markup += '<button type="' + el[0] + '">' + el[1] + '</button>';
+  if (tagName === "list") {
+    markup += '<li><h2 class="h2">' + el + "</h2></li>";
+  } else if (tagName === "link") {
+    markup += '<a href="' + el[1] + '">' + el[0] + "</a>";
+  } else if (tagName === "formTitle") {
+    markup += "<h2>" + el + "</h2>";
+  } else if (tagName === "formText") {
+    markup += "<p>" + el + "</p>";
+  } else if (tagName === "formFields") {
+    markup += "<h2>" + el[0] + "</h2>";
+    if (el[2] !== "textarea") {
+      markup +=
+        '<input type="' +
+        el[2] +
+        '" name="' +
+        el[3] +
+        '" placeholder="' +
+        el[1] +
+        '"/>';
+    } else {
+      markup += '<textarea rows=5 name="' + el[3] + '"></textarea>';
     }
-};
+  } else if (tagName === "addressPhone") {
+    markup += '<p><a href="tel:' + el + '">' + el + "</a></p>";
+  } else if (tagName === "addressEmail") {
+    markup += '<p><a href="mailto:' + el + '">' + el + "</a></p>";
+  } else if (tagName === "workHoursTitle") {
+    markup += "<caption>" + el + "</caption>";
+  } else if (tagName === "workHoursInfo") {
+    if (whi === 0) {
+      markup += "<tr><td>" + el + '</td><td rowspan="5">09.00-19.00</td></tr>';
+      whi++;
+    } else if (whi === 5) {
+      markup += "<tr><td>" + el + '</td><td rowspan="2">09.00-15.00</td></tr>';
+      whi++;
+    } else {
+      markup += "<tr><td>" + el + "</td></tr>";
+      whi++;
+    }
+  } else if ("button") {
+    markup += '<button type="' + el[0] + '">' + el[1] + "</button>";
+  }
+}
--- Logway/js/functions.js
+++ Logway/js/functions.js
@@ -1,95 +1,95 @@
 var workers = [
-    {
-        'name': 'Tadas Lekerauskas',
-        'phone': '+37062176494',
-        'email': 'tadas@logway1.lt',
-        'photo': 'img/img_red/logo_big_white.jpg',
-        'skype': ['leikeris', 'Tadas']
-    },
-    {
-        'name': 'Paulius Gučius',
-        'phone': '+37065552644',
-        'email': 'paulius@logway1.lt',
-        'photo': 'img/img_red/logo_big_white.jpg',
-        'skype': ['live:6ca4de6d0b8be973', 'Paulius']
-    },
-    {
-        'name': 'Ilona Zapolskaja',
-        'phone': '+37060541314',
-        'email': 'ilona@logway1.lt',
-        'photo': 'img/img_red/logo_big_white.jpg',
-        'skype': ['ilona-sakbar', 'Ilona']
-    },
-    {
-        'name': 'Maija Kilbovskaja',
-        'phone': '+37063119399',
-        'email': 'maija@logway1.lt',
-        'photo': 'img/img_red/logo_big_white.jpg',
-        'skype': ['Maija-Logway1', 'Maija']
-    }
-]
+  {
+    name: "Tadas Lekerauskas",
+    phone: "+37062176494",
+    email: "tadas@logway1.lt",
+    photo: "img/img_red/logo_big_white.jpg",
+    skype: ["leikeris", "Tadas"]
+  },
+  {
+    name: "Paulius Gučius",
+    phone: "+37065552644",
+    email: "paulius@logway1.lt",
+    photo: "img/img_red/logo_big_white.jpg",
+    skype: ["live:6ca4de6d0b8be973", "Paulius"]
+  },
+  {
+    name: "Ilona Zapolskaja",
+    phone: "+37060541314",
+    email: "ilona@logway1.lt",
+    photo: "img/img_red/logo_big_white.jpg",
+    skype: ["ilona-sakbar", "Ilona"]
+  },
+  {
+    name: "Maija Kilbovskaja",
+    phone: "+37063119399",
+    email: "maija@logway1.lt",
+    photo: "img/img_red/logo_big_white.jpg",
+    skype: ["Maija-Logway1", "Maija"]
+  }
+];
 
 // CREATING CONTACT CARDS
 
-var workersDiv = document.getElementsByClassName('workers')[0];
+var workersDiv = document.getElementsByClassName("workers")[0];
 
 for (var i = 0; i < workers.length; i++) {
-    var workMain = document.createElement('DIV');
-    workMain.setAttribute('class', 'worker');
-    var photo = document.createElement('IMG');
-    photo.setAttribute('src', workers[i]['photo']);
-    workersDiv.appendChild(workMain);
-    workMain.appendChild(photo);
-    photo.style.padding = '1em';
-    photo.style.borderRadius = '50%';
-    var workInfo = document.createElement('DIV');
-    workInfo.setAttribute('class', 'workerInfo');
-    workMain.appendChild(workInfo);
-    for (var key in workers[i]) {
-        if (workers[i].hasOwnProperty(key) && key != 'photo') {
-            let p = document.createElement("P");
-            workInfo.appendChild(p);
-            switch (key) {
-                case 'phone':
-                    let iconTel = document.createElement('IMG');
-                    iconTel.setAttribute('src', 'img/icon_phone.png');
-                    p.appendChild(iconTel);
-                    let aTel = document.createElement('A');
-                    let tel = 'tel: ' + workers[i][key];
-                    aTel.setAttribute('href', tel);
-                    p.appendChild(aTel);
-                    aTel.innerHTML = workers[i][key];
-                    break;
-                case 'email':
-                    let iconEmail = document.createElement('IMG');
-                    iconEmail.setAttribute('src', 'img/icon_email.png');
-                    p.appendChild(iconEmail);
-                    let aMail = document.createElement('A');
-                    let mail = 'mailto: ' + workers[i][key];
-                    aMail.setAttribute('href', mail);
-                    p.appendChild(aMail);
-                    aMail.innerHTML = workers[i][key];
-                    break;
-                case 'skype':
-                    let iconSkype = document.createElement('IMG');
-                    iconSkype.setAttribute('src', 'img/icon_skype.png');
-                    p.appendChild(iconSkype);
-                    let aSkype = document.createElement('A');
-                    let skype = 'skype:' + workers[i][key][0] + '?chat';
-                    aSkype.setAttribute('href', skype);
-                    p.appendChild(aSkype);
-                    aSkype.innerHTML = workers[i][key][1];
-                    break;
-                default:
-                    p.innerHTML = workers[i][key];
-                    break;
-            }
-        }
+  var workMain = document.createElement("DIV");
+  workMain.setAttribute("class", "worker");
+  var photo = document.createElement("IMG");
+  photo.setAttribute("src", workers[i]["photo"]);
+  workersDiv.appendChild(workMain);
+  workMain.appendChild(photo);
+  photo.style.borderRadius = "50%";
+  var workInfo = document.createElement("DIV");
+  workInfo.setAttribute("class", "workerInfo");
+  workMain.appendChild(workInfo);
+  for (var key in workers[i]) {
+    if (workers[i].hasOwnProperty(key) && key != "photo") {
+      let p = document.createElement("P");
+      workInfo.appendChild(p);
+      switch (key) {
+        case "phone":
+          let iconTel = document.createElement("IMG");
+          iconTel.setAttribute("src", "img/icon_phone.png");
+          p.appendChild(iconTel);
+          let aTel = document.createElement("A");
+          let tel = "tel: " + workers[i][key];
+          aTel.setAttribute("href", tel);
+          p.appendChild(aTel);
+          aTel.innerHTML = workers[i][key];
+          break;
+        case "email":
+          let iconEmail = document.createElement("IMG");
+          iconEmail.setAttribute("src", "img/icon_email.png");
+          p.appendChild(iconEmail);
+          let aMail = document.createElement("A");
+          let mail = "mailto: " + workers[i][key];
+          aMail.setAttribute("href", mail);
+          p.appendChild(aMail);
+          aMail.innerHTML = workers[i][key];
+          break;
+        case "skype":
+          let iconSkype = document.createElement("IMG");
+          iconSkype.setAttribute("src", "img/icon_skype.png");
+          p.appendChild(iconSkype);
+          let aSkype = document.createElement("A");
+          let skype = "skype:" + workers[i][key][0] + "?chat";
+          aSkype.setAttribute("href", skype);
+          p.appendChild(aSkype);
+          aSkype.innerHTML = workers[i][key][1];
+          break;
+        default:
+          p.innerHTML = workers[i][key];
+          break;
+      }
     }
+  }
 }
 var date = new Date();
 var year = date.getFullYear();
-document.getElementById('copyright').innerHTML = 'Visos teisės saugomos &copy; ' + year + ' Logway1';
+document.getElementById("copyright").innerHTML =
+  "Visos teisės saugomos &copy; " + year + " Logway1";
 
 // var listCust = document.getElementsByClassName('customs')[0].getElementsByTagName('LI');
 // var listWare = document.getElementsByClassName('warehouse')[0].getElementsByTagName('LI');
