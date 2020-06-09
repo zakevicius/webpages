921ac5bafc3c0525dbba913389a5c53745961820
zakevicius <m.zakevicius@gmail.com>
2020-06-09 18:42:24 +0300

    Logway v2 initial

---
+++ Logway_v2/webpack.dev.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const config = require('./webpack.config');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+
+module.exports = merge(config, {
+  mode: "development",
+  devtool: '#cheap-module-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      minimize: false
+    }),
+    new ErrorOverlayPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  }
+})
