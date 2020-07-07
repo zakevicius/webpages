1eae775aa3fad76a50cff317f70ab88c77c43e07
Marius Žakevičius <marius.zakevicius@alchemetrics.co.uk>
2020-07-07 21:53:51 +0300

    Header styling

--- zindymokelias/front/src/components/api/api.js
+++ zindymokelias/front/src/components/api/api.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios';
 
-const local = "http://localhost:8080";
-const heroku = "https://zindymo.herokuapp.com/";
+const local = 'http://localhost:8080';
+const heroku = 'https://zindymo.herokuapp.com/';
 
 export default axios.create({
-	baseURL: local,
+	baseURL: heroku
 });
